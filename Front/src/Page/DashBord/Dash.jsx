96043e5216b43ee4a60ceb7f5ef9e26ff16e2fd9
abdelhadi boudjemline <abdouhadi2002@gmail.com>
2024-02-29 20:45:42 +0100

    git abdelhadi

--- Front/src/Page/DashBord/Dash.jsx
+++ Front/src/Page/DashBord/Dash.jsx
@@ -1,9 +1,22 @@
+import { Link } from "react-router-dom";
 import NavBar from "../../components/NavBar/NavBar";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faCommentDots} from "@fortawesome/free-solid-svg-icons";
+import "../DashBord/Dash.css"
 export default function Dash() {
     return(
         <div>
               <NavBar/>
+              <div className="mainDash" style={{position:'relative'}}>
+                  hh
+
+
+            <Link to={'/message'} className="Chat" >
+                <div className="DivChat">
+                    <FontAwesomeIcon icon={faCommentDots} size="xl"  color="white"/>
+                </div>
+             </Link>
+              </div>
         </div>
     )
 }
